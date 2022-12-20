8b377542914ed464539c6136c0b81f738e0e70fe
「Mouse」Killer <119196998+MouseKillerLK@users.noreply.github.com>
2022-12-20 18:20:27 +0800

    Add files via upload

---
+++ join the qq group.cpp
@@ -0,0 +1,6 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	system("start https://qun.qq.com/qqweb/qunpro/share?_wv=3&_wwv=128&appChannel=share&biz=ka&businessType=5&from=246611&inviteCode=1XJwOfNWZst&mainSourceId=qr_code&subSourceId=pic4&jumpsource=shorturl#/pc");
+	return 0;
+} 
